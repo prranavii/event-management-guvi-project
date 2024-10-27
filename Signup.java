7e4694c7e9f298ece3cf89b5dc7c62e5fcc2a5aa
Pranavi <147311008+prranavii@users.noreply.github.com>
2024-10-27 16:17:21 +0530

    Add files via upload

---
+++ Signup.java
@@ -0,0 +1,24 @@
+public class Signup {
+    private Long id;
+
+    private String username;
+    private String email;
+    private String phoneNumber;
+
+
+    private String role;
+
+    private String password;
+
+    public Signup() {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.phoneNumber =phoneNumber;
+        this.role = role;
+        this.password = password;
+
+    }
+
+}
+
